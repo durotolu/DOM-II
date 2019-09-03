89c10ccf832f98ba7687801f7350669d95b141df
Modurotolu Olokode <durolawk@gmail.com>
2019-09-03 15:44:08 +0100

    wheel done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+
+// mouseover
+// keydown
+// wheel
+// drag / drop
+// load
+// focus
+// resize
+// scroll
+// select
+// dblclick
+
+let navigation = document.querySelectorAll('.nav-link');
+for (let i=0; i<navigation.length; i++) {
+    navigation[i].addEventListener('mouseover', (e) => e.target.style.color = 'blue');
+}
+for (let i=0; i<navigation.length; i++) {
+    navigation[i].addEventListener('mouseover', (e) => e.target.style.fontWeight = 'bold');
+}
+for (let i=0; i<navigation.length; i++) {
+    navigation[i].addEventListener('mouseout', (e) => e.target.style.color = 'initial');
+}
+for (let i=0; i<navigation.length; i++) {
+    navigation[i].addEventListener('mouseout', (e) => e.target.style.fontWeight = 'initial');
+}
+
+let keyChange = document.querySelector('*');
+keyChange.addEventListener('keydown', (e) => e.target.style.color = 'blue');
+
+let roll = document.querySelector('*');
+roll.addEventListener('wheel', () => alert("You're scrolling"));
+
